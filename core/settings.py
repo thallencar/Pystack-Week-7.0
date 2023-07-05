48e2728b1cf9b521ab5a9e29d31cd64148e77be7
Thalita Alencar <125590093+thallencar@users.noreply.github.com>
2023-07-04 23:04:37 -0300

    correção de arquivos deletados

--- core/settings.py
+++ core/settings.py
@@ -117,6 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT = os.path.join('static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
